d87eb0e525cbf5d82aa06384d2dee2b386ba37a4
islamdev96 <islamsayedglab@gmail.com>
2023-08-11 11:35:37 +0300

    ..

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
 });
 
 
-Route::get('/dashboard', [DashboardController::class ,"index"]);
+Route::get('/dashboard', array(DashboardController::class, 'index'));
